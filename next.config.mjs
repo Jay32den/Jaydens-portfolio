cd4e5dace95738e4f8c5f87001ce7eaee39bb1a7
jay32den <siskjayden09@gmail.com>
2024-07-17 23:59:47 -0500

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "images.pexels.com",
+        },
+        {
+            protocol: "https",
+            hostname: "m.media-amazon.com",
+          },
+          {
+            protocol: "https",
+            hostname: "m.media-amazon.com",
+          },
+          {
+            protocol: "https",
+            hostname: "m.media-amazon.com",
+          },
+      ],
+    },
+  };
+  
+  export default nextConfig;
